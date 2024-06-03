2faf2a00f7b9286c722e83a31b15aa2f706c53df
Alexander <158214122+AlexIvanovu@users.noreply.github.com>
2024-06-03 14:40:06 +0300

    Update IvanovJava1.java

--- IvanovJava1.java
+++ IvanovJava1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercise_1_1 {
+public class IvanovJava1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Задание 1.1.");
