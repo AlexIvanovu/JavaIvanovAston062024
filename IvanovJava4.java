e2dd982466685636399d71a6397ec1b83ecf5006
Alexander <158214122+AlexIvanovu@users.noreply.github.com>
2024-06-03 14:48:54 +0300

    Create IvanovJava4.java

---
+++ IvanovJava4.java
@@ -0,0 +1,12 @@
+public class IvanovJava4 {
+    public static void main(String[] args) {
+        System.out.println("Задание 2.3.");
+        int [] ch = {1,2,3,4,5,6,7,8,9,10};
+        int i;
+        for (i=0; i<ch.length; i++) {
+            if (ch[i] % 2 == 0) {
+                System.out.println(ch[i]);
+            }
+        }
+    }
+}
