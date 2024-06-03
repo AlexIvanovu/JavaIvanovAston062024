4ae52cff73a63b83f15b16050a2edd97638a97aa
Alexander <158214122+AlexIvanovu@users.noreply.github.com>
2024-06-03 14:43:45 +0300

    Update IvanovJava1.java

--- IvanovJava1.java
+++ IvanovJava1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class IvanovJava1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Задание 1.1.");
+        System.out.println("Задание 2.1.1.");
         System.out.println("Написать программу, которая принимает на вход два целых числа (a и b) и совершает с ними следующие действия:");
         System.out.println("сравнивает эти два числа и возвращает результат сравнения путем вывода в консоль одного из вариантов: a > b, a < b или a = b");
         System.out.println("Введите число a");
